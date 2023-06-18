7ce090ba9248daebfdca943a9573b45934b8ad61
Dimitri Harding <vaughnharding@hotmail.com>
2023-06-18 10:32:10 -0500

    refactor: redirect on web too

--- components/auth-form.tsx
+++ components/auth-form.tsx
@@ -14,7 +14,7 @@ export default function AuthForm() {
       theme="dark"
       showLinks={false}
       providers={[]}
-      redirectTo="http://localhost:3000/auth/callback"
+      redirectTo={`${process.env.NEXT_PUBLIC_APP_URL}/auth/callback`}
     />
   );
 }
